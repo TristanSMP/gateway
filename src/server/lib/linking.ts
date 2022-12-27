6e1d209c5d4f0d29b8b30a463204adebb7a9d965
Tristan Camejo <contact@tristancamejo.com>
2022-12-27 23:51:07 +1100

    feat: sync user

--- src/server/lib/linking.ts
+++ src/server/lib/linking.ts
@@ -1,3 +1,45 @@
+import type { Application, User } from "@prisma/client";
+import { ApplicationStatus } from "@prisma/client";
+import { elytra } from "../db/client";
+
 export function generateLinkChallenge(): string {
   return Math.random().toString(36).slice(2, 7).toUpperCase();
+}
+
+export async function syncUser(
+  user: User & {
+    application: Application | null;
+  }
+): Promise<void> {
+  if (!user.minecraftUUID || !user.application) {
+    throw new Error("User not linked or has no application");
+  }
+
+  const actions: Promise<unknown>[] = [];
+
+  switch (user.application.status) {
+    case ApplicationStatus.Approved:
+      actions.push(elytra.lp.addPermission(user.minecraftUUID, "group.active"));
+      break;
+    default:
+      actions.push(
+        elytra.lp.removePermission(user.minecraftUUID, "group.active")
+      );
+      break;
+  }
+
+  try {
+    await Promise.all(actions);
+  } catch (error) {
+    console.log(
+      [
+        `Failed to sync user ${user.id} with UUID ${user.minecraftUUID}`,
+        `Application status: ${user.application.status}`,
+        `Error:`,
+      ].join("\n"),
+      error
+    );
+
+    throw error;
+  }
 }
