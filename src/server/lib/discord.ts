5800c3c565719bfbd687ea3400356e20e5befa66
Tristan Camejo <contact@tristancamejo.com>
2022-12-28 21:27:42 +1100

    fix: perms

--- src/server/lib/discord.ts
+++ src/server/lib/discord.ts
@@ -106,6 +106,17 @@ export async function createApplicationChannel(
           type: OverwriteType.Member,
           allow: `${1 << 10}`,
         },
+        {
+          id: env.DISCORD_GUILD_ID,
+          type: OverwriteType.Role,
+          deny: `${1 << 10}`,
+        },
+        {
+          // TODO(tristan): make this an env var
+          id: "1054688510784839751",
+          type: OverwriteType.Role,
+          allow: `${1 << 10}`,
+        },
       ],
     })
   );
