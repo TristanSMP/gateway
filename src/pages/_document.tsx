ae290f4d1445970be19fafd0c84c9e109ad41443
Tristan Camejo <contact@tristancamejo.com>
2022-12-31 09:40:53 +1100

    feat(web): new theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html className="dark h-full">
+    <Html className="dark h-full" data-theme="dracula">
       <Head />
       <body className="h-full">
         <Main />
