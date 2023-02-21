cc6226f4a27e6d7710f1d39101d01bcf131000cd
Tristan Camejo <contact@tristancamejo.com>
2023-02-22 07:53:51 +1100

    fix(bot): refresh

--- src/bot/commands/refresh.ts
+++ src/bot/commands/refresh.ts
@@ -4,6 +4,7 @@ import {
 } from "discord-api-types/v10";
 import type { ChatInputInteraction, Command } from "disploy";
 import { updateRoleMeta } from "../../server/lib/discord";
+import { getTSMPUser } from "../../server/lib/utils";
 
 const Refresh: Command = {
   name: "refresh",
@@ -19,10 +20,18 @@ const Refresh: Command = {
 
   async run(interaction: ChatInputInteraction) {
     const user = interaction.options.getUser("user");
+    const tsmpUser = await getTSMPUser(user.id).catch(() => null);
+
+    if (!tsmpUser) {
+      return void interaction.reply({
+        content: "Failed to find linked TSMP user!",
+        flags: MessageFlags.Ephemeral,
+      });
+    }
 
     await updateRoleMeta(user.id);
 
-    interaction.reply({ content: "Refreshed!", flags: MessageFlags.Ephemeral });
+    interaction.reply({ content: "Refreshed!" });
   },
 };
 
