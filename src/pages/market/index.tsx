8b1c080994ef9912ce12d90a402a06f59315fe22
Tristan Camejo <contact@tristancamejo.com>
2023-03-03 11:55:18 +1100

    refactor(market): material

--- src/pages/market/index.tsx
+++ src/pages/market/index.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
-import { DocumentIcon } from "@heroicons/react/24/solid";
-import { List, Modal } from "@mantine/core";
+import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
 import { showNotification } from "@mantine/notifications";
+import * as Mui from "@mui/material";
 import { TRPCClientError } from "@trpc/client";
 import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
-import Link from "next/link";
 import { useState } from "react";
 import Inventory from "../../components/player/Inventory";
 import type { InventorySlotPayload } from "../../server/lib/market/serialization";
@@ -98,80 +97,125 @@ const Market: NextPage = () => {
 
   return (
     <>
-      <Modal
-        opened={sellItemModalOpened}
+      <Mui.Dialog
+        open={sellItemModalOpened}
         onClose={() => setItemModalOpened(false)}
-        title="Sell item"
+        aria-labelledby="form-dialog-title"
       >
-        <div className="flex flex-col gap-2">
-          <input
+        <Mui.DialogTitle id="form-dialog-title">Sell item</Mui.DialogTitle>
+        <Mui.DialogContent>
+          <Mui.DialogContentText>
+            Enter the price you want to sell the item for
+          </Mui.DialogContentText>
+          <Mui.TextField
+            autoFocus
+            margin="dense"
+            id="price"
+            label="Price"
             type="number"
+            fullWidth
             value={price}
             onChange={(e) => setPrice(Number(e.target.value))}
           />
-          <button onClick={handleItemSell}>Sell</button>
-        </div>
-      </Modal>
+        </Mui.DialogContent>
+        <Mui.DialogActions>
+          <Mui.Button onClick={() => setItemModalOpened(false)} color="primary">
+            Cancel
+          </Mui.Button>
+          <Mui.Button onClick={handleItemSell} color="primary">
+            Sell
+          </Mui.Button>
+        </Mui.DialogActions>
+      </Mui.Dialog>
 
-      <button onClick={handleDeposit}>Deposit diamonds</button>
+      <Mui.Container>
+        <Mui.Grid container spacing={3}>
+          <Mui.Grid item xs={12}>
+            <Mui.Typography variant="h3" component="h3">
+              you have {balanceQuery.data ?? "Loading..."} diamonds
+            </Mui.Typography>
+            <Mui.Button onClick={handleDeposit}>Deposit diamonds</Mui.Button>
+          </Mui.Grid>
+          <Mui.Grid item xs={12}>
+            <Mui.Typography variant="body1" component="p">
+              This is your current inventory (or an error message if you&apos;re
+              not online), click items to publish them.{" "}
+              <span className="text-red-500">
+                warning: you probably don&apos;t want to do that, the market is
+                a work in progress and you may lose your items
+              </span>
+            </Mui.Typography>
+          </Mui.Grid>
+          <Mui.Grid item xs={12}>
+            {inventoryQuery.isLoading && <div>Loading...</div>}
+            {inventoryQuery.isError && <div>Error</div>}
+            {inventoryQuery.data && (
+              <Inventory
+                inventory={inventoryQuery.data}
+                onClick={handleItemClick}
+              />
+            )}
+          </Mui.Grid>
+          <Mui.Grid item xs={12}>
+            <Mui.Typography variant="h2" component="h2">
+              Discovered Items
+            </Mui.Typography>
+          </Mui.Grid>
+          <Mui.Grid item xs={12}>
+            <Mui.Typography variant="body1" component="p">
+              These are the items the market has &quot;discovered&quot;. Items
+              are discovered from the first time a unique item is published.
+            </Mui.Typography>
+          </Mui.Grid>
 
-      <h1 className="mb-4 text-4xl font-bold">Market</h1>
+          <Mui.Grid item xs={12}>
+            {discoveredItemTypesQuery.isLoading && <div>Loading...</div>}
+            {discoveredItemTypesQuery.isError && <div>Error</div>}
+            {discoveredItemTypesQuery.data && (
+              <Mui.TableContainer component={Mui.Paper}>
+                <Mui.Table aria-label="simple table">
+                  <Mui.TableHead>
+                    <Mui.TableRow>
+                      <Mui.TableCell>Item</Mui.TableCell>
+                      <Mui.TableCell align="right">
+                        Cheapest Price
+                      </Mui.TableCell>
+                      <Mui.TableCell align="right">Sellers</Mui.TableCell>
+                    </Mui.TableRow>
+                  </Mui.TableHead>
+                  <Mui.TableBody>
+                    {discoveredItemTypesQuery.data.map((item, id) => (
+                      <Mui.TableRow key={id}>
+                        <Mui.TableCell component="th" scope="row">
+                          <Mui.Icon className="mr-2">
+                            {item.image ? (
+                              <img src={item.image} alt={item.name} />
+                            ) : (
+                              <QuestionMarkCircleIcon />
+                            )}
+                          </Mui.Icon>
 
-      <h2 className="mb-4 text-2xl font-bold">Inventory</h2>
+                          <Mui.Link href={`/market/${item.id}`}>
+                            {item.name}
+                          </Mui.Link>
+                        </Mui.TableCell>
 
-      <h3 className="mb-4 text-2xl font-bold">
-        you have {balanceQuery.data ?? "LOADINGGGGG"} diamonds
-      </h3>
+                        <Mui.TableCell align="right">
+                          {item.cheapest?.price ?? "N/A"} diamonds
+                        </Mui.TableCell>
 
-      <p className="mb-4">
-        This is your current inventory (or an error message if you&apos;re not
-        online), click items to publish them.{" "}
-        <span className="text-red-500">
-          warning: you probably don&apos;t want to do that, the market is a work
-          in progress and you may lose your items
-        </span>
-      </p>
-
-      {inventoryQuery.isLoading && <div>Loading...</div>}
-
-      {inventoryQuery.isError && <div>Error</div>}
-
-      {inventoryQuery.data && (
-        <Inventory inventory={inventoryQuery.data} onClick={handleItemClick} />
-      )}
-
-      <h2 className="mb-4 text-2xl font-bold">Discovered Items</h2>
-
-      <p className="mb-4">
-        These are the items the market has &quot;discovered&quot;. Items are
-        discovered from the first time a unique item is published.
-      </p>
-
-      {discoveredItemTypesQuery.isLoading && <div>Loading...</div>}
-      {discoveredItemTypesQuery.isError && <div>Error</div>}
-      {discoveredItemTypesQuery.data && (
-        <List>
-          {discoveredItemTypesQuery.data.map((item, id) => (
-            <List.Item
-              icon={
-                item.image ? (
-                  <div className="flex flex-row">
-                    <img src={item.image} alt={item.name} />
-                    <span className="ml-2 ">{item.sellers.length} sellers</span>
-                  </div>
-                ) : (
-                  <DocumentIcon />
-                )
-              }
-              key={id}
-            >
-              <Link href={`/market/${item.id}`} className="hover:underline">
-                {item.name}
-              </Link>
-            </List.Item>
-          ))}
-        </List>
-      )}
+                        <Mui.TableCell align="right">
+                          {item.sellers.length}
+                        </Mui.TableCell>
+                      </Mui.TableRow>
+                    ))}
+                  </Mui.TableBody>
+                </Mui.Table>
+              </Mui.TableContainer>
+            )}
+          </Mui.Grid>
+        </Mui.Grid>
+      </Mui.Container>
     </>
   );
 };
