35be58e451688140fee9fb03610c349ecd407a42
Tristan Camejo <contact@tristancamejo.com>
2023-03-05 18:48:13 +1100

    refactor(market/item): mui

--- src/pages/market/[id].tsx
+++ src/pages/market/[id].tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { showNotification } from "@mantine/notifications";
+import * as Mui from "@mui/material";
 import { TRPCClientError } from "@trpc/client";
 import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
@@ -48,116 +49,129 @@ const MarketItem: NextPage = () => {
   };
 
   return itemQuery.data ? (
-    <div>
-      <div className="pt-6">
-        {/* Image gallery */}
-        <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-          <div className="aspect-w-3 aspect-h-4 hidden overflow-hidden rounded-lg lg:block">
-            <img
-              src={itemQuery.data.image}
-              className="h-full w-full object-cover object-center"
-              style={{
+    <Mui.Container>
+      <Mui.Grid container spacing={3}>
+        <Mui.Grid item xs={12} md={6}>
+          <Mui.Card>
+            <Mui.CardMedia
+              component="img"
+              image={itemQuery.data.image}
+              title={itemQuery.data.name}
+              sx={{
                 imageRendering: "pixelated",
               }}
-              alt={itemQuery.data.name}
             />
-          </div>
-        </div>
+          </Mui.Card>
+        </Mui.Grid>
+        <Mui.Grid item xs={12} md={6}>
+          <Mui.Badge
+            badgeContent="Cheapest"
+            color="primary"
+            className="md:w-full"
+          >
+            <Mui.Card sx={{ width: "100%" }}>
+              <Mui.CardContent>
+                <Mui.Typography variant="h4" component="h1">
+                  {itemQuery.data.amount > 1
+                    ? `${itemQuery.data.amount} x `
+                    : ""}{" "}
+                  {itemQuery.data.name}
+                </Mui.Typography>
 
-        {/* Product info */}
-        <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
-          <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-            <h1 className="text-2xl font-bold tracking-tight  sm:text-3xl">
-              {itemQuery.data.name}
-            </h1>
-          </div>
-
-          {/* Options */}
-          <div className="mt-4 lg:row-span-3 lg:mt-0">
-            <h2 className="sr-only">Product information</h2>
-            <p className="text-3xl tracking-tight">
-              {itemQuery.data.cheapest
-                ? `${itemQuery.data.cheapest.price} diamonds`
-                : "No sellers!"}
-            </p>
-
-            <form className="mt-10">
-              <button
-                type="submit"
-                className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                onClick={(e) => {
-                  e.preventDefault();
-                  if (!itemQuery.data.cheapest)
-                    return showNotification({
-                      message: "No sellers!",
-                      color: "red",
-                    });
-                  buyItem(itemQuery.data.cheapest.id);
-                }}
-                disabled={!itemQuery.data.cheapest}
-              >
-                {itemQuery.data.cheapest ? (
-                  <>Buy from cheapest seller ({itemQuery.data.cheapest.name})</>
-                ) : (
-                  <>No sellers!</>
+                {itemQuery.data.enchantments.length > 0 && (
+                  <Mui.Card elevation={2} sx={{ p: 2 }}>
+                    {itemQuery.data.enchantments.map((enchantment) => (
+                      <Mui.Chip label={enchantment} key={enchantment} />
+                    ))}
+                  </Mui.Card>
                 )}
-              </button>
-            </form>
-          </div>
 
-          <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pb-16 lg:pr-8">
-            {/* Description and details */}
-            <div>
-              <h3 className="sr-only">Description</h3>
-
-              <div className="space-y-6">
-                <p className="text-base">{itemQuery.data.type}</p>
-              </div>
-            </div>
-
-            {/* other sellers */}
-            <div className="mt-10">
-              <h3 className="sr-only">Other sellers</h3>
-
-              <table className="w-full">
-                <thead>
-                  <tr>
-                    <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider ">
-                      Seller
-                    </th>
-                    <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider ">
-                      Price
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y">
-                  {itemQuery.data.sellers.map((seller) => (
-                    <tr key={seller.id}>
-                      <td className="whitespace-nowrap px-6 py-4 text-sm">
-                        {seller.name}
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4 text-sm">
-                        {seller.price} diamonds{" "}
-                        <button
-                          className="text-indigo-600 hover:text-indigo-900"
-                          onClick={() => {
-                            buyItem(seller.id);
-                          }}
-                        >
-                          buy
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+                <Mui.Typography variant="h5" component="h2">
+                  {itemQuery.data.cheapest
+                    ? `${itemQuery.data.cheapest.price} diamonds`
+                    : "No sellers!"}
+                </Mui.Typography>
+                <Mui.Button
+                  variant="contained"
+                  color="primary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    if (!itemQuery.data.cheapest)
+                      return showNotification({
+                        message: "No sellers!",
+                        color: "red",
+                      });
+                    buyItem(itemQuery.data.cheapest.id);
+                  }}
+                  disabled={!itemQuery.data.cheapest}
+                >
+                  {itemQuery.data.cheapest ? (
+                    <>
+                      Buy from cheapest seller ({itemQuery.data.cheapest.name})
+                    </>
+                  ) : (
+                    <>No sellers!</>
+                  )}
+                </Mui.Button>
+              </Mui.CardContent>
+            </Mui.Card>
+          </Mui.Badge>
+        </Mui.Grid>
+        <Mui.Grid item xs={12}>
+          <Mui.Card>
+            <Mui.CardContent>
+              <Mui.Typography variant="h5" component="h3">
+                Other sellers
+              </Mui.Typography>
+              <Mui.TableContainer>
+                <Mui.Table>
+                  <Mui.TableHead>
+                    <Mui.TableRow>
+                      <Mui.TableCell>Seller</Mui.TableCell>
+                      <Mui.TableCell>Price</Mui.TableCell>
+                      <Mui.TableCell></Mui.TableCell>
+                    </Mui.TableRow>
+                  </Mui.TableHead>
+                  <Mui.TableBody>
+                    {itemQuery.data.sellers.map((seller) => (
+                      <Mui.TableRow key={seller.id}>
+                        <Mui.TableCell>{seller.name}</Mui.TableCell>
+                        <Mui.TableCell>{seller.price} diamonds</Mui.TableCell>
+                        <Mui.TableCell>
+                          <Mui.Button
+                            variant="contained"
+                            color="primary"
+                            onClick={() => {
+                              buyItem(seller.id);
+                            }}
+                          >
+                            Buy
+                          </Mui.Button>
+                        </Mui.TableCell>
+                      </Mui.TableRow>
+                    ))}
+                  </Mui.TableBody>
+                </Mui.Table>
+              </Mui.TableContainer>
+            </Mui.CardContent>
+          </Mui.Card>
+        </Mui.Grid>
+      </Mui.Grid>
+    </Mui.Container>
   ) : (
-    <>things are loading</>
+    <Mui.Container>
+      <Mui.Grid container spacing={3}>
+        <Mui.Grid item xs={12}>
+          <Mui.Card>
+            <Mui.CardContent>
+              <Mui.Typography variant="h5" component="h3">
+                Loading...
+              </Mui.Typography>
+            </Mui.CardContent>
+          </Mui.Card>
+        </Mui.Grid>
+      </Mui.Grid>
+    </Mui.Container>
   );
 };
 
