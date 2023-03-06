e625d692a98ae0d443848d730b5db9245f45a12f
Tristan Camejo <contact@tristancamejo.com>
2023-03-07 10:56:31 +1100

    feat(bot): suggest motd

--- src/bot/commands/_router.ts
+++ src/bot/commands/_router.ts
@@ -9,6 +9,7 @@ import Ping from "./ping";
 import Refresh from "./refresh";
 import ResetUser from "./reset-user";
 import UserStats from "./stats";
+import SuggestMOTD from "./suggest-motd";
 
 export const Commands = [
   Ping,
@@ -22,4 +23,5 @@ export const Commands = [
   OnlinePlayers,
   InDiscordApply,
   MarketLeaderboard,
+  SuggestMOTD,
 ];
---
+++ src/bot/commands/suggest-motd.ts
@@ -0,0 +1,82 @@
+import { ApplicationStatus } from "@prisma/client";
+import type { RESTPostAPIChannelMessageJSONBody } from "discord-api-types/v10";
+import {
+  ApplicationCommandOptionType,
+  MessageFlags,
+  Routes,
+} from "discord-api-types/v10";
+import type { ChatInputInteraction, Command } from "disploy";
+import { env } from "../../env/server.mjs";
+import { getTSMPUser } from "../../server/lib/utils";
+import { EmbedColor } from "../utils/embeds.js";
+
+const SuggestMOTD: Command = {
+  name: "suggest-motd",
+  description: "Suggest an MOTD",
+  options: [
+    {
+      name: "line1",
+      description: "The first line of the MOTD",
+      type: ApplicationCommandOptionType.String,
+      required: true,
+      max_length: 30,
+    },
+    {
+      name: "line2",
+      description: "The second line of the MOTD",
+      type: ApplicationCommandOptionType.String,
+      required: false,
+      max_length: 30,
+    },
+  ],
+
+  async run(interaction: ChatInputInteraction) {
+    const tsmpUser = await getTSMPUser(interaction.user.id).catch(() => null);
+
+    if (
+      !tsmpUser ||
+      tsmpUser.application?.status !== ApplicationStatus.Approved
+    ) {
+      return void interaction.reply({
+        content: "You are not a member.",
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+
+    const line1 = interaction.options.getString("line1", false);
+    const line2 = interaction.options.getString("line2", true);
+
+    await interaction.app.rest.post<RESTPostAPIChannelMessageJSONBody, unknown>(
+      Routes.channelMessages(env.DISCORD_REVIEW_QUEUE_CHANNEL_ID),
+      {
+        content: `<@&${env.DISCORD_REVIEW_ROLE_ID}>`,
+        embeds: [
+          {
+            title: "New MOTD",
+            description: [
+              `Submitted by ${interaction.user}`,
+              "```",
+              `TS ${line1}`,
+              `MP ${line2 ?? ""}`,
+              "```",
+            ].join("\n"),
+            color: EmbedColor.Invisible,
+          },
+        ],
+      }
+    );
+
+    interaction.reply({
+      embeds: [
+        {
+          title: "Submitted MOTD",
+          description:
+            "Your MOTD has been submitted. It will be considered by tristan himself.",
+          color: EmbedColor.Invisible,
+        },
+      ],
+    });
+  },
+};
+
+export default SuggestMOTD;
