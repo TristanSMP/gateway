01b90bdca7d4126c23fec070ea64f7f35058bd6f
Tristan Camejo <contact@tristancamejo.com>
2023-03-22 09:11:22 +1100

    fix(blog): text sizing

--- src/components/blog/BlogPageInfo.tsx
+++ src/components/blog/BlogPageInfo.tsx
@@ -1,16 +1,25 @@
 import type { IBlogPost } from "../../server/lib/blog/utils";
 import Authors from "./Authors";
+import BlogTag from "./BlogTag";
 
 const BlogPageInfo: React.FC<{
   post: IBlogPost;
 }> = ({ post }) => {
   return (
     <div className="flex flex-col">
-      <h1 className="text-7xl font-bold">{post.title}</h1>
+      <h1 className="text-center text-2xl font-bold lg:text-7xl">
+        {post.title}
+      </h1>
       <div className="flex flex-row items-center justify-evenly p-5">
         <p className="text-gray-500">
           {new Date(post.createdAt).toLocaleDateString()}
         </p>
+
+        <div className="flex flex-row items-center justify-evenly space-x-4 p-5">
+          {post.tags.map((tag) => (
+            <BlogTag tag={tag} key={tag.name} />
+          ))}
+        </div>
 
         <Authors authors={post.authors} playerAuthors={post.players} />
       </div>
